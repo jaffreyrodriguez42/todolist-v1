5251b5d06076578f172a434195dd35b8de080d03
Jaff Rey Rodriguez <jaffreyrodriguez42@gmail.com>
2020-06-22 16:15:53 +0800

    Passing Data from Your Webpage to Your Server

--- app.js
+++ app.js
@@ -2,40 +2,32 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+var works = [];
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs"); //ejs - Embedded JavaScript Templating
 
 app.get("/", function (req, res) {
   var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 1:
-      day = "Tuesday";
-      break;
-    case 1:
-      day = "Wednesday";
-      break;
-    case 1:
-      day = "Thursday";
-      break;
-    case 1:
-      day = "Friday";
-      break;
-    case 1:
-      day = "Saturday";
-      break;
-    default:
-      console.log("Error: current day is equal to: " + currentDay);
-  }
-  res.render("list", { kindOfDay: day });
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+
+  var day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", { kindOfDay: day, works: works });
+});
+
+app.post("/", function (req, res) {
+  var work = req.body.work;
+  works.push(work);
+  res.redirect("/");
+  // console.log(work);
+  // res.render("list", { work: work });
 });
 
 app.listen(3000, function () {
