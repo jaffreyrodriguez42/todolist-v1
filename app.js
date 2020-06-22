f4a2b39ad749ae8750911dc35ab46f2544770e8e
Jaff Rey Rodriguez <jaffreyrodriguez42@gmail.com>
2020-06-22 12:09:09 +0800

    What We'll Make: A ToDoList

--- app.js
+++ app.js
@@ -4,7 +4,19 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("Hello!");
+  var today = new Date();
+  var currentDay = today.getDay();
+
+  if (currentDay === 6 || currentDay === 0) {
+    // res.send("<h1>Yay it's the weekend!</h1>");
+    res.write("<h1>Yay it's the weekend!</h1>");
+  } else {
+    res.sendFile(__dirname + "/index.html");
+    // res.write("<p>It is not the weekend!</p>");
+    // res.write("<h1>Boo! I have to work</h1>");
+    // res.send("<h1>Boo! I have to work</h1>");
+    // res.send();
+  }
 });
 
 app.listen(3000, function () {
