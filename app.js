676f3f6c40215ad6016f059ab51d4c022ccf3d38
Jaff Rey Rodriguez <jaffreyrodriguez42@gmail.com>
2020-06-26 18:18:44 +0800

    Revisiting Lodash and Deleting Items from Custom ToDo Lists

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const _ = require("lodash");
 
 
 const app = express();
@@ -86,16 +87,25 @@ app.post("/", function (req, res) { //post request to the Home route
 
 app.post("/delete", function (req, res) {
   const checkedItemId = req.body.checkBox;
-  console.log(checkedItemId);
-  Item.findByIdAndRemove(checkedItemId, function (err) {
-    if (!err) {
-      console.log("Deleted!");
-    } else {
-      console.log(err);
-    }
-    res.redirect("/");
+  const listName = req.body.listName;
 
-  });
+  if (listName === "Today") {
+    Item.findByIdAndRemove(checkedItemId, function (err) {
+      if (!err) {
+        console.log("Deleted!");
+      } else {
+        console.log(err);
+      }
+      res.redirect("/");
+
+    });
+  } else {
+    List.findOneAndUpdate({ name: listName }, { $pull: { items: { _id: checkedItemId } } }, function (err, foundList) {
+      if (!err) {
+        res.redirect("/" + listName);
+      }
+    });
+  }
 
 });
 
@@ -105,7 +115,7 @@ app.post("/delete", function (req, res) {
 // });
 
 app.get("/:customListName", function (req, res) {
-  const customListName = req.params.customListName;
+  const customListName = _.capitalize(req.params.customListName);
   List.findOne({ name: customListName }, function (err, foundList) {
     if (!err) {
       if (!foundList) {
